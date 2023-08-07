5dafa4101e99d33b07696c17680138f021e41d3f
MoeShair <hamoda.tam@gmail.com>
2023-08-07 10:30:59 +0300

    table page logic ready

--- myscripts.js
+++ myscripts.js
@@ -1,8 +1,16 @@
 let URL = "http://universities.hipolabs.com/search?name=a"
 
-const main = document.getElementById('list-container')
+const listContainer = document.getElementById('list-container')
 
-const whishSet = new Set();
+let whishSet = ''
+
+if (localStorage != null) {
+    whishSet = new Set(JSON.parse(localStorage.getItem("whishSet")));
+}
+else {
+    whishSet = new Set();
+}
+
 /*
 let response = fetch(URL)
 .then(res => res.json())
@@ -23,7 +31,7 @@ function fillingHtml() {
                     
                      </div>`
         }
-        main.innerHTML = univ*/
+        listContainer.innerHTML = univ*/
 
         const listContainer = document.getElementById("list-container")
         for (let i = 0; i < re.length; i++) {
@@ -86,16 +94,92 @@ document.addEventListener('DOMContentLoaded', function () {
         let searchInput = document.getElementById("my-input").value
         console.log("U are in the function")
         URL = `http://universities.hipolabs.com/search?name=${searchInput}`
-        main.innerHTML=""
+        listContainer.innerHTML = ""
         fillingHtml()
     })
 
-   
+
 });
 function myFunction() {
     console.log("Yooo")
     let x = document.getElementById("side-filter").value
     URL = `http://universities.hipolabs.com/search?country=${x}`
-    main.innerHTML=""
+    listContainer.innerHTML = ""
     fillingHtml()
+}
+
+function tablepage() {
+
+    console.log("memmememeemmememe")
+    document.getElementById("main").removeChild(listContainer)
+    //document.getElementById("side-nav").removeChild(document.getElementById("side-filter"))
+    //listContainer.innerHTML =""
+    document.getElementById("page2").style.color = "red"
+    tableContainer = document.createElement("div");
+    tableContainer.id = "table-container";
+
+    const tablee = document.createElement("table")
+
+    const headerRow = document.createElement("tr")
+
+    const tableheader = document.createElement("th")
+    tableheader.textContent = "Name"
+    headerRow.appendChild(tableheader)
+
+    const tableheader1 = document.createElement("th")
+    tableheader1.textContent = "Country"
+    headerRow.appendChild(tableheader1)
+
+    const tableheader2 = document.createElement("th")
+    tableheader2.textContent = "Checked"
+    headerRow.appendChild(tableheader2)
+
+    const tableheader3 = document.createElement("th")
+    tableheader3.textContent = "Remove"
+    headerRow.appendChild(tableheader3)
+
+    tablee.appendChild(headerRow)
+
+    tableContainer.appendChild(tablee)
+    document.getElementById("main").appendChild(tableContainer);
+
+    let tableItems = JSON.parse(localStorage.getItem("whishSet"))
+    for (let i = 0; i < tableItems.length; i++) {
+        console.log("yoo")
+        let tableRow = document.createElement("tr")
+
+        let td = document.createElement("td")
+        td.textContent = tableItems[i]['name']
+        tableRow.appendChild(td)
+
+        let td1 = document.createElement("td")
+        td1.textContent = tableItems[i]['country']
+        tableRow.appendChild(td1)
+
+        let td2 = document.createElement("td")
+        let Checked = document.createElement("button")
+        Checked.textContent = "check"
+        td2.appendChild(Checked)
+        tableRow.appendChild(td2)
+
+        let td3 = document.createElement("td")
+        let remove = document.createElement("button")
+        remove.textContent = "remove"
+        td3.appendChild(remove)
+        tableRow.appendChild(td3)
+
+
+        tablee.appendChild(tableRow)
+
+        remove.addEventListener("click", () => {
+            tablee.removeChild(tableRow)
+            tableItems.splice(i, 1)
+            localStorage.setItem("whishSet", JSON.stringify(tableItems))
+        })
+        Checked.addEventListener("click", ()=>{
+            tableRow.style.backgroundColor = "green"
+            Checked.disabled = "true"
+            Checked.textContent = "checked 😊"
+        })
+    }
 }
